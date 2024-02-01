953b1c36e4e02ab22cf1f65297f1586b93232578
Alon Itzhaki <alon.itzhaki@zerto.com>
2024-02-01 18:47:59 +0200

    adding a_star rta

--- Probelm.py
+++
@@ -1,8 +0,0 @@
-from package_graph import package_graph
-
-class Problem(object):
-    def __init__(self, path, goalstate, graph):
-        self.Initial_State = package_graph(path)
-
-        self.Initial_State.g = 0
-        self.Goal_State = goalstate
